2bc246d47afe4ace2b034f585170d7088cbf22e7
Peter Andras Lukacs <peter.lukacs@emarsys.com>
2018-03-23 13:10:56 +0100

    switch to shiny dashboard

--- ui.R
+++ ui.R
@@ -1,27 +1,86 @@
-shinyUI(fluidPage(
+library("shinydashboard")
 
-    title = "Lakhely Kereso",
+header <- dashboardHeader(
+    title = "Re-engager"
+)
 
-    sidebarLayout(
+sidebar <- dashboardSidebar(
+    sidebarMenu(
+        id = "main_tabs",
 
-    # Sidebar panel for inputs ----
-        sidebarPanel(
-
-      # Input: Slider for the number of bins ----
-            sliderInput(inputId = "bins",
-                        label = "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-
+        menuItem(
+            "plots", tabName = "plots", icon = icon("tachometer")
+            , selected = TRUE
         ),
 
-        mainPanel(
-            tabsetPanel(type = "tabs",
-                        tabPanel("Ahol vagyok"),
+        menuItem(
+            "reports", tabName = "reports", icon = icon("file")
+            , selected = FALSE
+        ),
 
-                        tabPanel("Hova menjek?")
+        dateInput(
+            inputId = 'start_date',
+            label = h4('start date'),
+            value = as.Date('2018-02-06'),
+            max = Sys.Date() - 7
+        ),
+
+        textInput(
+            inputId = 'customer_name',
+            label = h4('customer name'),
+            value = 'edigital'
+        ),
+
+        checkboxInput(
+            inputId = 'refetch_checkbox',
+            label = 'refetch data'
+        ),
+
+        actionButton(
+            inputId = 'run',
+            label = 'Run'
+        )
+    )
+)
+
+body <- dashboardBody(
+    tabItems(
+        tabItem(
+            "plots",
+            fluidRow(
+                plotDownloadUI("opened_percent_of_delivered"),
+                plotDownloadUI("fslo_distribution")
+            ),
+            fluidRow(
+                plotDownloadUI("last_send_histogram"),
+                plotDownloadUI("eps_distribution")
+            ),
+            fluidRow(
+                plotDownloadUI("weekly_campaigns_open_rate", boxwidth = 12)
+            ),
+            fluidRow(
+                plotDownloadUI("last_click_histogram")
+            ),
+            fluidRow(
+                plotDownloadUI("weekly_campaigns_click_rate", boxwidth = 12)
+            )
+        ),
+        tabItem(
+            "reports",
+            # fluidRow(
+            #     downloadButton("downloadReport", label = "Download")
+            # ),
+            fluidRow(
+                uiOutput("markdown")
             )
         )
     )
-))
+)
+
+dashboardPage(
+    header,
+    sidebar,
+    body,
+    skin = "blue",
+    title = "Re-engager custom report"
+)
